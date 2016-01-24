5db4379958a22378c073bb8bf40b92273afeca43
lev2048 <lev@shield.asia>
2016-01-24 21:46:37 +0800

    C#调用摄像头

---
+++ Csharp调用摄像头/FrmMain/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FrmMain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(567, 190);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "button1";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(58, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 289);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 458);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Csharp调用摄像头/FrmMain/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
+
+namespace FrmMain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //声明变量
+        public FilterInfoCollection USE_Webcams = null;
+        public VideoCaptureDevice cam = null;
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btn_Start.Text == "开始")
+                {
+                    ///--  
+                    btn_Start.Text = "停止";
+                    ///---启动摄像头  
+                    cam.Start();
+                }
+                else
+                {
+                    ///--设置按钮提示字样  
+                    btn_Start.Text = "开始";
+                    ///--停止摄像头捕获图像  
+                    cam.Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ///---实例化对象  
+                USE_Webcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                ///---摄像头数量大于0  
+                if (USE_Webcams.Count > 0)
+                {
+                    ///---禁用按钮  
+                    btn_Start.Enabled = true;
+                    ///---实例化对象  
+                    cam = new VideoCaptureDevice(USE_Webcams[0].MonikerString);
+                    ///---绑定事件  
+                    cam.NewFrame += new NewFrameEventHandler(Cam_NewFrame);
+                }
+                else
+                {
+                    ///--没有摄像头  
+                    btn_Start.Enabled = false;
+                    ///---提示没有摄像头  
+                    MessageBox.Show("没有摄像头外设");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Cam_NewFrame(object obj, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            ///---throw new NotImplementedException();  
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                if (cam != null)
+                {
+                    ///---关闭摄像头  
+                    if (cam.IsRunning)
+                    {
+                        cam.Stop();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+   
+}
