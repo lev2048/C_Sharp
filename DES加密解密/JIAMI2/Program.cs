2be1dd6d377bf42926f51f8e31b51ffaeb1c082c
lev2048 <lev@shield.asia>
2016-01-29 20:07:18 +0800

    DES加密解密

---
+++ DES加密解密/JIAMI2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentManager;
+
+namespace JIAMI2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string srr, str = "123";
+            srr = StringSecurity.DESEncrypt(str);
+            Console.WriteLine("{0}", srr);         
+            //string swr, ser = "";
+            //swr = StringSecurity.DESDecrypt(ser);
+            //Console.WriteLine("{0}", swr);
+            Console.ReadKey();
+        }
+    }
+}
