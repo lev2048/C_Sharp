7f60f91b87f981a5f0872d66ee1b109bb2da2c67
lev2048 <lev@shield.asia>
2016-02-12 23:56:17 +0800

    查询考试成绩信息

--- 学员信息管理系统/DAL/ScoreListService.cs
+++ 学员信息管理系统/DAL/ScoreListService.cs
@@ -14,9 +14,35 @@ namespace DAL
     /// </summary>
     public class ScoreListService
     {
+        /// <summary>
+        /// 查询考试成绩信息
+        /// </summary>
+        /// <param name="className"></param>
+        /// <returns></returns>
         public List<StudentExt> GetScoreList(string className)
         {
-            string sql="select StudentName,Students.StudentId ClassName,CSharp,SQLServerDB from"
+            string sql = "select StudentName,Students.StudentId ClassName,CSharp,SQLServerDB from Students";
+            sql += " inner join StudentClass on Students.ClassId=StudentClass.ClassId";
+            sql += " inner join ScoreList on Students.Students.StudentId=ScoreList.StudentId";
+            if (className != null && className.Length != 0)
+            {
+                sql += string.Format(" where ClassName='{0}'",className);
+            }
+            SqlDataReader objReader = SQLHelper.GetReader(sql);
+            List<StudentExt> list = new List<StudentExt>();
+            while(objReader.Read())
+            {
+                list.Add(new StudentExt()
+                {
+                    StudentId = Convert.ToInt32(objReader["StudentId"]),
+                    StudentName = objReader["StudentName"].ToString(),
+                    ClassName = objReader["ClassName"].ToString(),
+                    CSharp = objReader["CSharp"].ToString(),
+                    SQLServerDB = objReader["SQLSever"].ToString()
+                });
+            }
+            objReader.Close();
+            return list;
         }
     }
 }
