a0051edb70c42fc186d3e8a9c230f8a9416eff38
lev2048 <lev@shield.asia>
2016-02-12 12:59:58 +0800

    更新

--- 学员信息管理系统/DAL/ScoreListService.cs
+++ 学员信息管理系统/DAL/ScoreListService.cs
@@ -5,12 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using Models;
+using Models.Ext;
 namespace DAL
 {
     /// <summary>
-    /// 成绩
+    /// 成绩管理数据访问类
     /// </summary>
     public class ScoreListService
     {
+        public List<StudentExt> GetScoreList(string className)
+        {
+            string sql="select StudentName,Students.StudentId ClassName,CSharp,SQLServerDB from"
+        }
     }
 }
